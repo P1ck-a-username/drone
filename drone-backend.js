7a88b96b23021f5e40ffd04b9bed504bcbfc0772
V3512 (Conference ID) <v3512@cam.ac.uk>
2015-08-14 12:27:31 +0100

    123

--- drone-backend.js
+++ drone-backend.js
@@ -1,6 +1,7 @@
 var Cylon = require('cylon');
+var utils = require('utils/droneUtils.js');
 var bot;
-
+var landed = true;
 // Initialise the robot
 Cylon.robot()
     .connection("ardrone", {
@@ -11,11 +12,60 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    landed = false;
+
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data) {
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+            landed = true;
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+
+    });
+
+    if (landed == false) {
+        after(5 * 1000, function () {
+            bot.drone.left(0.2);
+        });
+        after(6 * 1000, function () {
+            bot.drone.forward(0.2);
+        });
+        after(7 * 1000, function () {
+            bot.drone.right(0.2);
+        });
+        after(8 * 1000, function () {
+            bot.drone.back(0.2);
+        });
+        after(9 * 1000, function () {
+            bot.drone.stop();
+            landed = true;
+        });
+    }
+    bot.drone.getPngStream()
+        .on("data", utils.sendFrame);
 
 }
 
 Cylon.start();
+
--- drone-frontend.js
+++ drone-frontend.js
@@ -1,20 +1,79 @@
 function init() {
-    tracker = initTracker("#example");
+    tracker = initTracker("#droneView");
+    droneConnection.streamImage(tracker,"#droneView.drone");
     tracking.track("#example .drone", tracker);
 }
 
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+
+    TrackerUtils.addTrackingColor("#5EA24E", "green", tracker);
+
     TrackerUtils.startTrackingColors(tracker);
 
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
-        console.log(event.data);
+        markColors(event.data, element);
+        decideDroneMovement(event.data);
     });
 
     return tracker;
+}
+
+function markColors(colors, element) {
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+
+    context.clearRect(0,0,context.width,context.length);
+
+    for (var i=0; i<colors.length; i++) {
+        drawRectangle(colors[i], context);
+        writeRectangle(colors[i], element + ' .output')
+    }
+}
+
+function drawRectangle(rect, context) {
+    if (rect.width * rect.height > 2000) {
+        context.strokeStyle = rect.color;
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    }
+}
+
+function writeRectangle(rect, element) {
+    if (rect.width * rect.height > 2000) {
+        $(element)
+            .append("<p>")
+            .append(rect.color + ": " + rect.width + "X" + rect.height)
+            .append(" @ " + rect.x + ":" + rect.y)
+    }
+}
+
+function decideDroneMovement(colors){
+    var move = {
+        left: false,
+        right: false
+    };
+
+    for(var i = 0; i < colors.length; i++) {
+        var rectangle = colors[i];
+        var colour = checkColor(rectangle);
+        if (colour == "green") {
+            if (rectangle.width > 300) {
+                move.left = true;
+            }
+        }
+        if (colour == "red") {
+            if (rectangle.width > 300) {
+                move.right = ture;
+            }
+        }
+    }
+    droneConnection.send(move);
+}
+
+function checkColor(rectangle) {
+    return rectangle.color;
 }
 
 window.addEventListener("load", init);
